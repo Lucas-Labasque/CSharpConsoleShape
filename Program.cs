9e27e247ddbaddf1386e279527dce5e98f948475
Lucas-Labasque <lucas.labasque@viacesi.fr>
2023-03-10 14:09:55 +0100

    push

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿  // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using CSharpConsoleShape.Models;
+using CSharpConsoleShape.Models.Forms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+public class AreaCalculator
+{
+    public double TotalArea(Shape[] arrShapes)
+    {
+        double area = 0;
+        foreach (var objShape in arrShapes)
+        {
+            area += objShape.Area();
+        }
+        return area;
+    }
+}
+
